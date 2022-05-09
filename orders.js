38eab8de297922b04e78d2218290e0163a57414b
jonathanppmaia <jonathanppmaia@gmail.com>
2022-05-09 17:23:53 -0300

    Envio de Arquivos

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+  {
+    productName: 'Roku Fu pro Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Peding'
+  },
+  {
+    productName: ' LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Roku F11 pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Peding'
+  },
+  {
+    productName: 'Drone with Camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '22821',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  }
+]
